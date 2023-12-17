8ee361193163a817e67092eaf8ad55dce1cf1cd6
Daniel San <106621051+DanielBlem93@users.noreply.github.com>
2023-12-17 14:48:20 +0100

    end project

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,3 @@ import { AppModule } from './app/app.module';
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
-console.log(`
-Moin ein paar kurze Infos für den der es kontrolliert:
--In der Portfolio section sind die Projekte doppelt zu finden. Die werden natürlich ersetzt
-mit den zukünftigen Angular Projekten und dienen nur als Platzhalter.
-
-`)
