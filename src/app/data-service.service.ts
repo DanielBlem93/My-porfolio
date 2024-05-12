5a72a9d4f03512d6976351bd348ea2531887e487
Daniel San <106621051+DanielBlem93@users.noreply.github.com>
2024-05-12 21:16:30 +0200

    added da bubble

--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -21,7 +21,7 @@ export class DataServiceService {
         name: 'El Pollo loco',
         image: '../../assets/img/projects/Pollo loco 1.svg',
         description: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and salsa bottles to fight against the giant Chicken.',
-        technologies: ['JavaScript', ' HTML', ' CSS'],
+        technologies: ['JavaScript | HTML | CSS |'],
         githubLink: 'https://github.com/DanielBlem93/el-pollo-loco',
         projectUrl: 'https://daniel-blem.de/el-pollo-loco/index.html',
       },
@@ -29,27 +29,18 @@ export class DataServiceService {
         name: 'Join',
         image: '../../assets/img/projects/join.svg',
         description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-        technologies: ['JavaScript', ' HTML', ' CSS'],
+        technologies: ['JavaScript | HTML | CSS'],
         githubLink: 'https://github.com/DanielBlem93/join',
         projectUrl: 'https://daniel-blem.de/join/index.html'
       },
       {
-        name: 'El Pollo loco',
-        image: '../../assets/img/projects/Pollo loco 1.svg',
-        description: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and salsa bottles to fight against the giant Chicken.',
-        technologies: ['JavaScript', ' HTML', ' CSS'],
-        githubLink: 'https://github.com/DanielBlem93/el-pollo-loco',
-        projectUrl: 'https://daniel-blem.de/el-pollo-loco/index.html',
+        name: 'DABubble',
+        image: '../../assets/img/projects/dabubble-lappi.png ',
+        description: 'A Slack clone app that facilitates smooth team communication and collaboration through messaging, file sharing, and customizable channels to enhance workplace efficiency.',
+        technologies: ['Angular | TypeScript | HTML | SCSS | Firebase '],
+        githubLink: 'https://github.com/DanielBlem93/DA-Bubble',
+        projectUrl: 'https://daniel-blem.de/angular-projects/dabubble/index.html',
       },
-      {
-        name: 'Join',
-        image: '../../assets/img/projects/join.svg',
-        description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-        technologies: ['JavaScript', ' HTML', ' CSS'],
-        githubLink: 'https://github.com/DanielBlem93/join',
-        projectUrl: 'https://daniel-blem.de/join/index.html'
-      },
-
     ];
   }
 }
