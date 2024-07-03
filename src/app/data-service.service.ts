846952ffba17fe440c88d17cdecbfcc57cf13654
Daniel San <106621051+DanielBlem93@users.noreply.github.com>
2024-07-03 23:06:24 +0200

    added pokedex

--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -18,10 +18,18 @@ export class DataServiceService {
   constructor() {
     this.projectList = [
       {
+        name: 'DABubble',
+        image: '../../assets/img/projects/dabubble-lappi.png ',
+        description: 'A Slack clone app that facilitates smooth team communication and collaboration through messaging, file sharing, and customizable channels to enhance workplace efficiency.',
+        technologies: ['Angular | TypeScript | HTML | SCSS | Firebase '],
+        githubLink: 'https://github.com/DanielBlem93/DA-Bubble',
+        projectUrl: 'https://dabubble.daniel-blem.de/singup',
+      },
+      {
         name: 'El Pollo loco',
         image: '../../assets/img/projects/Pollo loco 1.svg',
         description: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and salsa bottles to fight against the giant Chicken.',
-        technologies: ['JavaScript | HTML | CSS |'],
+        technologies: ['JavaScript | HTML | CSS '],
         githubLink: 'https://github.com/DanielBlem93/el-pollo-loco',
         projectUrl: 'https://el-pollo-loco.daniel-blem.de/index.html',
       },
@@ -33,13 +41,14 @@ export class DataServiceService {
         githubLink: 'https://github.com/DanielBlem93/join',
         projectUrl: 'https://join.daniel-blem.de/index.html'
       },
+
       {
-        name: 'DABubble',
-        image: '../../assets/img/projects/dabubble-lappi.png ',
-        description: 'A Slack clone app that facilitates smooth team communication and collaboration through messaging, file sharing, and customizable channels to enhance workplace efficiency.',
-        technologies: ['Angular | TypeScript | HTML | SCSS | Firebase '],
-        githubLink: 'https://github.com/DanielBlem93/DA-Bubble',
-        projectUrl: 'https://dabubble.daniel-blem.de/singup',
+        name: 'Pokedex',
+        image: '../../assets/img/projects/pokedex.png ',
+        description: 'This Pokedex web app provides information about Pokémon, sourced from the PokéAPI.',
+        technologies: ['JavaScript | HTML | CSS '],
+        githubLink: 'https://github.com/DanielBlem93/Pokedex-with-JS',
+        projectUrl: 'https://pokedex.daniel-blem.de/index.html',
       },
     ];
   }
